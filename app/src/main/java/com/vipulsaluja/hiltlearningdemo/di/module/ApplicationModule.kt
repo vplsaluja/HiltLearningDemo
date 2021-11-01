bf3d60848e6bc919a7721e22cce2704af74958c8
Vipul Saluja <vplsaluja@gmail.com>
2021-11-01 12:41:32 +0530

    module refactored

--- app/src/main/java/com/vipulsaluja/hiltlearningdemo/di/module/ApplicationModule.kt
+++ app/src/main/java/com/vipulsaluja/hiltlearningdemo/di/module/ApplicationModule.kt
@@ -1,19 +1,55 @@
-/*
 package com.vipulsaluja.hiltlearningdemo.di.module
 
-import com.vipulsaluja.hiltlearningdemo.network.RetrofitClient
+import com.vipulsaluja.hiltlearningdemo.BuildConfig
+import com.vipulsaluja.hiltlearningdemo.BuildConfig.BASE_URL
+import com.vipulsaluja.hiltlearningdemo.network.NetworkApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 class ApplicationModule {
+
+//    private const val BASE_URL = "https://makeup-api.herokuapp.com/";
+
     @Provides
+    fun getBaseUrl()= BASE_URL
+
     @Singleton
-    fun getRetrofitClient():RetrofitClient(){
-        RetrofitClient
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun getRetrofitClient(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
     }
-}*/
+
+    @Singleton
+    @Provides
+    fun getNetworkApi(retrofit: Retrofit): NetworkApi = retrofit.create(NetworkApi::class.java)
+}
